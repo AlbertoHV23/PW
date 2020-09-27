267547c753a2e8c31981e424574064a718e96101
Alberto Hernandez <albertho0.hdzvqgmail.com>
2020-09-27 02:27:31 -0500

    Nuevos cambios, ahora si
    No se moverle a los branches y casi pierdo todo

--- cerra.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("iniciarform").addEventListener("click",function(){
-    document.querySelector(".popup").style.display ="flex";
-    document.querySelector("body .popup").style.display ="flex";
-});
-
